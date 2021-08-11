0ae0d1a39427a1cc773beb81935b26c21c872007
victordurco <victordurco@gmail.com>
2021-08-11 20:31:35 -0300

    refactor of create quizz page 1

--- scripts/createQuizz.js
+++ scripts/createQuizz.js
@@ -12,6 +12,40 @@ function isValidUrl(string) { //this function is from stackoverflow
     return url.protocol === "http:" || url.protocol === "https:";
  }
 
+function isValidPage1(){
+    //validade inputs page1
+    if (quizzImg==="" || quizzTitle==="" || nQuestions==="" || nLevels===""){
+        alert("Preencha os campos corretamente");
+        return false;
+    }else{
+        nQuestions = Number(nQuestions);
+        nLevels = Number(nLevels);
+        if(isNaN(nQuestions) || isNaN(nLevels)){
+            alert("A quantidade de questões e a quantidade de níveis devem ser números");
+            return false;
+        }
+        if(quizzTitle.length < 20 || quizzTitle.length > 65){
+            alert("O título deve conter entre 20 e 65 caracteres");
+            return false;
+        }
+        if(!isValidUrl(quizzImg)){
+            alert("Link da imagem inválido");
+            return false;
+        }
+        if(nQuestions < 3){
+            alert("O quizz deve conter pelo menos 3 questões");
+            return false;
+        }
+        if(nLevels < 2){
+            alert("O quizz deve conter pelo menos 2 níveis");
+            return false;
+        }
+
+        return true;
+    }
+}
+
+
 function loadCreatePage1(){
     document.querySelector(".home").style.display = "none";
     document.querySelector(".create-1").style.display = "flex";
@@ -23,35 +57,10 @@ function finishPage1(){
     nQuestions = document.getElementById("numberQuestions").value;
     nLevels = document.getElementById("numberLevels").value;
 
-    //validade inputs and load page 2
-    if (quizzImg==="" || quizzTitle==="" || nQuestions==="" || nLevels===""){
-        alert("Preencha os campos corretamente");
-        return;
-    }else{
-        nQuestions = Number(nQuestions);
-        nLevels = Number(nLevels);
-        if(isNaN(nQuestions) || isNaN(nLevels)){
-            alert("A quantidade de questões e a quantidade de níveis devem ser números");
-            return;
-        }
-        if(quizzTitle.length < 20 || quizzTitle.length > 65){
-            alert("O título deve conter entre 20 e 65 caracteres");
-            return;
-        }
-        if(!isValidUrl(quizzImg)){
-            alert("Link da imagem inválido");
-            return;
-        }
-        if(nQuestions < 3){
-            alert("O quizz deve conter pelo menos 3 questões");
-            return;
-        }
-        if(nLevels < 2){
-            alert("O quizz deve conter pelo menos 2 níveis");
-            return;
-        }
-
+    if (isValidPage1()){
         loadCreatePage2();
+    }else{
+        return;
     }
 }
 
