bf2eb01d0fbd9c809d710970e6fa2f8c1602b0a4
victordurco <victordurco@gmail.com>
2021-08-11 20:21:59 -0300

    create quizz page 1 validation done

---
+++ scripts/createQuizz.js
@@ -0,0 +1,64 @@
+let newQuizz, quizzQuestions = [];
+let quizzTitle, quizzImg = "";
+let nQuestions, nLevels = 0;
+
+function isValidUrl(string) { //this function is from stackoverflow
+    let url;
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;  
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
+ }
+
+function loadCreatePage1(){
+    document.querySelector(".home").style.display = "none";
+    document.querySelector(".create-1").style.display = "flex";
+}
+
+function finishPage1(){
+    quizzTitle = document.getElementById("newQuizzTitle").value;
+    quizzImg = document.getElementById("newQuizzImg").value;
+    nQuestions = document.getElementById("numberQuestions").value;
+    nLevels = document.getElementById("numberLevels").value;
+
+    //validade inputs and load page 2
+    if (quizzImg==="" || quizzTitle==="" || nQuestions==="" || nLevels===""){
+        alert("Preencha os campos corretamente");
+        return;
+    }else{
+        nQuestions = Number(nQuestions);
+        nLevels = Number(nLevels);
+        if(isNaN(nQuestions) || isNaN(nLevels)){
+            alert("A quantidade de questões e a quantidade de níveis devem ser números");
+            return;
+        }
+        if(quizzTitle.length < 20 || quizzTitle.length > 65){
+            alert("O título deve conter entre 20 e 65 caracteres");
+            return;
+        }
+        if(!isValidUrl(quizzImg)){
+            alert("Link da imagem inválido");
+            return;
+        }
+        if(nQuestions < 3){
+            alert("O quizz deve conter pelo menos 3 questões");
+            return;
+        }
+        if(nLevels < 2){
+            alert("O quizz deve conter pelo menos 2 níveis");
+            return;
+        }
+
+        loadCreatePage2();
+    }
+}
+
+function loadCreatePage2(){
+    console.log(quizzTitle);
+    console.log(quizzImg);
+    console.log(nQuestions);
+    console.log(nLevels);
+}
+
