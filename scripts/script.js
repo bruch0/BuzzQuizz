674bbf59334c067baa69edaef87d755e90d69458
bruch0 <lucas.bruch0@gmail.com>
2021-08-11 10:17:18 -0300

    added the all quizzes section

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+const URL_API = 'https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/';
+
+function getQuizzes () {
+    let promise = axios.get(URL_API + 'quizzes')
+    .then(renderQuizzes);
+    // .catch(error);
+}
+
+function renderQuizzes(response) {
+    console.log(response.data);
+}
