e31013b008a2416b29a14bf1fe82b04e9d37a972
bruch0 <lucas.bruch0@gmail.com>
2021-08-12 14:10:35 -0300

    fixed issues on quizzes that dont have the usual amount of answers

--- scripts/quizzing.js
+++ scripts/quizzing.js
@@ -17,28 +17,23 @@ function renderQuizzing(response) {
     document.querySelector('.quizzing-header p').innerHTML = response.data.title;
 
     for (let i = 0; i < response.data.questions.length; i++) {
+        let nButtons = response.data.questions[i].answers.length;
+        let buttons = '';
+        for (let j = 0; j < nButtons; j++) {
+            buttons += `<div>
+                            <button class="unset" id="img-1"></button>
+                            <p class="unset"></p>
+                        </div>`
+        }
+        
+
         document.querySelector('section.quizzing').innerHTML += 
         `<div class="question">
             <div class="container">
                 <p class="title unset"></p>
             
                 <div class="options">
-                    <div>
-                        <button class="unset" id="img-1"></button>
-                        <p class="unset"></p>
-                    </div>
-                    <div>
-                        <button class="unset" id="img-2"></button>
-                        <p class="unset"></p>
-                    </div>
-                    <div>
-                        <button class="unset" id="img-3"></button>
-                        <p class="unset"></p>
-                    </div>
-                    <div>
-                        <button class="unset" id="img-4"></button>
-                        <p class="unset"></p>
-                    </div>
+                    ${buttons}
                 </div>
             </div>
         </div>`
@@ -49,10 +44,11 @@ function renderQuizzing(response) {
 }
 
 function renderQuestions (response) {
+    console.log(response.data);
     for (let i = 0; i < response.data.questions.length; i++) {
         document.querySelector('.question .title.unset').innerHTML = response.data.questions[i].title;
         document.querySelector('.question .title.unset').classList.remove('unset');
-        for (let j = 0; j < 4 ; j++) {
+        for (let j = 0; j < response.data.questions[i].answers.length ; j++) {
             document.querySelector('.question .options p.unset').innerHTML = response.data.questions[i].answers[j].text;
             document.querySelector('.question .options p.unset').classList.remove('unset');
     
